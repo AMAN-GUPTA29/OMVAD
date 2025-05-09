b2fbf729ee7fa0f4499b5b682a21e4a4b4491d15
AMAN-GUPTA29 <aman.g21@iiits.in>
2025-05-10 02:12:29 +0530

    styleerrorr

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -5,7 +5,7 @@ import axios from 'axios';
  * @type {import('axios').AxiosInstance}
  */
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000',
+  baseURL: 'http://localhost:5000/api',
   headers: {
     'Content-Type': 'application/json',
   },
