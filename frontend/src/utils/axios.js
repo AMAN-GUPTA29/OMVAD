c8001d6ba819477ecbbd85cd217fc8b5936c620b
AMAN-GUPTA29 <aman.g21@iiits.in>
2025-05-10 02:25:14 +0530

    styleerrorr

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -5,7 +5,7 @@ import axios from 'axios';
  * @type {import('axios').AxiosInstance}
  */
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://omvad.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
